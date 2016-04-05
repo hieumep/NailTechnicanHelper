c938505ede3fa3805c9009a22689771a83c7a5d5
RonaldVo <hieuvo@gmail.com>
2016-04-04 23:22:33 -0700

    add data

---
+++ Nail Technican Helper/CoreDataStackManager.swift
@@ -0,0 +1,9 @@
+//
+//  CoreDataStackManager.swift
+//  Nail Technican Helper
+//
+//  Created by Hieu Vo on 4/4/16.
+//  Copyright © 2016 Hieu Vo. All rights reserved.
+//
+
+import Foundation
---
+++ Nail Technican Helper/NailShop.swift
@@ -0,0 +1,9 @@
+//
+//  NailShop.swift
+//  Nail Technican Helper
+//
+//  Created by Hieu Vo on 4/4/16.
+//  Copyright © 2016 Hieu Vo. All rights reserved.
+//
+
+import Foundation
