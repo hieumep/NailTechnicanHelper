038545121327decb57f76e38030b6a6f1b085d50
RonaldVo <hieuvo@gmail.com>
2016-04-08 00:15:38 -0700

    present data on List shop table view

---
+++ Nail Technican Helper/NailShopTableViewController.swift
@@ -0,0 +1,9 @@
+//
+//  NailShopTableViewController.swift
+//  Nail Technican Helper
+//
+//  Created by Hieu Vo on 4/6/16.
+//  Copyright © 2016 Hieu Vo. All rights reserved.
+//
+
+import Foundation
