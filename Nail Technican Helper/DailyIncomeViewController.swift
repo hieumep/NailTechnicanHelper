e2ef084c2d7e8d90d97d8c13dd14977f020c3bcf
RonaldVo <hieuvo@gmail.com>
2016-04-10 23:09:38 -0700

    get current shop

--- Nail Technican Helper/DailyIncomeViewController.swift
+++ Nail Technican Helper/DailyIncomeViewController.swift
@@ -7,13 +7,44 @@
 //
 
 import UIKit
+import CoreData
 
 class DailyIncomeViewController : UIViewController {
     
     @IBOutlet weak var pickShopLabel: UILabel!
+    @IBOutlet weak var shopLabel: UILabel!
     
+    @IBOutlet weak var percentLabel: UILabel!
+    var shops :[NailShop]? = [NailShop]()
+    override func viewWillAppear(animated: Bool) {
+        getCurrentShop()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         pickShopLabel.text = " You don't set up Nail Shop yet, please tap EDIT to pick your current Nail Shop"
+      //  getCurrentShop()
+    }
+    
+    func getCurrentShop (){
+        let fetchRequest = NSFetchRequest(entityName: "NailShop")
+        fetchRequest.predicate = NSPredicate(format: "selected==true")
+        do {
+            shops = try sharedContext.executeFetchRequest(fetchRequest) as? [NailShop]
+        if let shops = shops {
+            shopLabel.text = shops[0].nailShop
+            percentLabel.text = "\(shops[0].percent)"
+        }
+        }catch{
+            print(error)
+            abort()
+        }
+    }
+    
+    lazy var sharedContext : NSManagedObjectContext = {
+        return CoreDataStackManager.sharedInstance().managerObjectContext
+    }()
+    
+    func saveContext(){
+        CoreDataStackManager.sharedInstance().saveContext()
     }
 }
