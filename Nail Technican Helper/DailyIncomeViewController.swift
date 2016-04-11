9802a260d6ebc8146cf23f5316b929f6e4c7405e
RonaldVo <hieuvo@gmail.com>
2016-04-10 23:31:52 -0700

    fix condition of current shop

--- Nail Technican Helper/DailyIncomeViewController.swift
+++ Nail Technican Helper/DailyIncomeViewController.swift
@@ -14,15 +14,17 @@ class DailyIncomeViewController : UIViewController {
     @IBOutlet weak var pickShopLabel: UILabel!
     @IBOutlet weak var shopLabel: UILabel!
     
+    @IBOutlet weak var mainView: UIStackView!
+    
     @IBOutlet weak var percentLabel: UILabel!
-    var shops :[NailShop]? = [NailShop]()
+    var shops :[NailShop]?
     override func viewWillAppear(animated: Bool) {
         getCurrentShop()
     }
     override func viewDidLoad() {
         super.viewDidLoad()
         pickShopLabel.text = " You don't set up Nail Shop yet, please tap EDIT to pick your current Nail Shop"
-      //  getCurrentShop()
+      
     }
     
     func getCurrentShop (){
@@ -30,10 +32,15 @@ class DailyIncomeViewController : UIViewController {
         fetchRequest.predicate = NSPredicate(format: "selected==true")
         do {
             shops = try sharedContext.executeFetchRequest(fetchRequest) as? [NailShop]
-        if let shops = shops {
-            shopLabel.text = shops[0].nailShop
-            percentLabel.text = "\(shops[0].percent)"
-        }
+        if shops?.count >= 1  {
+            shopLabel.text = shops![0].nailShop
+            percentLabel.text = "\(shops![0].percent)"
+            mainView.hidden = false
+            pickShopLabel.hidden = true
+        }else {
+            mainView.hidden = true
+            pickShopLabel.hidden = false
+            }
         }catch{
             print(error)
             abort()
